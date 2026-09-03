415427e9bc3fb3526376584958571bb7705fcdce
lyubenov83 <104453881+lyubenov83@users.noreply.github.com>
2026-09-03 15:30:24 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const form = document.querySelector("form");
-const formMessage = document.querySelector("#formMessage");
-
-form.addEventListener("submit", function(event){
-    event.preventDefault();
-
-    formMessage.textContent = "Информацията за връзка е попълнена успешно.";
-});
